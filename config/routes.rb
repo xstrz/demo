a038562e391c784960fb3ccaedbb4161b6b8064f
Vadim Belovalov <vadim.belovalov@gmail.com>
2010-12-10 12:18:51 +0500

    Added user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Demo::Application.routes.draw do
-  get "blogs/index"
-
-  get "blogs/show"
+  devise_for :users
 
   resources :posts
-  resources :blogs
-  
-  get "posts/show"
   
   #match '/new',    :to => 'posts#new'
   #match '/show',   :to => 'posts#show'
