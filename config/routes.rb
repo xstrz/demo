c576bac70a27eaa1b939db2d6d57a86a46e0f615
xstrz <vadim.belovalov@gmail.com>
2010-12-11 23:39:18 +0500

    add some string

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
-Demo::Application.routes.draw do
-  #get "pages/show"
+Demo::Application.routes.draw do  
+  #match '/authors/:id', :to => 'pages#show'
 
   devise_for :users
 
   resources :posts, :pages
-  
   
   #match '/user-post',    :to => 'posts#new'
   #match '/show',   :to => 'posts#show'
